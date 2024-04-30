c837894a7e5d22e78576ddb7fdceab967cad17ee
Gabriel Karczewski <nickelghost@outlook.com>
2024-04-30 19:47:00 +0200

    allow client side nav revert

--- src/react/GeneratorResult.tsx
+++ src/react/GeneratorResult.tsx
@@ -1,9 +1,14 @@
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 
 function GeneratorResult() {
   const { name } = useParams<{ name: string }>();
 
-  return <h2>Hello {name}!</h2>;
+  return (
+    <div>
+      <h2>Hello {name}!</h2>
+      <Link to="/generator">Back</Link>
+    </div>
+  );
 }
 
 export default GeneratorResult;
